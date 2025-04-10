e86bb01b4d0f3cea1c7dc34315a1ca75f4921336
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-04-10 14:06:07 -0400

    Simple API

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Api = require('./models/api.js');
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -16,4 +17,39 @@ app.use(express.json());
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
+});
+
+// CREATE - POST - /api
+// app.get('/api', async (req, res) => {
+//     res.send("test")
+
+// });
+
+// CREATE - POST - /api
+app.post('/api', async (req, res) => {
+    const createdApi = await Api.create(req.body);
+    res.json(createdApi)
+  });
+  
+  // READ - GET - /pets
+app.get('/api', async (req, res) => {
+	// Add a message to test the route
+	const foundApi = await Api.find();
+    res.json(foundApi);
+});
+
+app.delete('/api/:apiId', async (req, res) => {
+	// Add a message to test the route
+	const deletedApi = await Api.findByIdAndDelete(req.params.apiId);
+    res.json(deletedApi);
+});
+
+// UPDATE - PUT - /api/:apiId
+app.put('/:apiId', async (req, res) => {
+    // Add { new: true } as the third argument
+    const updatedApi = await Api.findByIdAndUpdate(
+	    req.params.apiId, 
+	    req.body,
+	    {new: true}
+    );
 });
